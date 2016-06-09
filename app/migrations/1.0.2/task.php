ec1c6f8d5eadc5c3cbe328dd4b7528b50913473e
Pavel Babushkin <darkdarin@mail.ru>
2016-06-09 14:13:47 +0500

    migration 1.0.2

---
+++ app/migrations/1.0.2/task.php
@@ -0,0 +1,143 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class TaskMigration_102
+ */
+class TaskMigration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('task', array(
+                'columns' => array(
+                    new Column(
+                        'id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        )
+                    ),
+                    new Column(
+                        'user_id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'id'
+                        )
+                    ),
+                    new Column(
+                        'status',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 2,
+                            'after' => 'user_id'
+                        )
+                    ),
+                    new Column(
+                        'reason',
+                        array(
+                            'type' => Column::TYPE_TEXT,
+                            'size' => 1,
+                            'after' => 'status'
+                        )
+                    ),
+                    new Column(
+                        'comment',
+                        array(
+                            'type' => Column::TYPE_TEXT,
+                            'size' => 1,
+                            'after' => 'reason'
+                        )
+                    ),
+                    new Column(
+                        'finished_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'comment'
+                        )
+                    ),
+                    new Column(
+                        'created_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'finished_at'
+                        )
+                    ),
+                    new Column(
+                        'updated_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'created_at'
+                        )
+                    ),
+                    new Column(
+                        'deleted_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'size' => 1,
+                            'after' => 'updated_at'
+                        )
+                    )
+                ),
+                'indexes' => array(
+                    new Index('PRIMARY', array('id'), 'PRIMARY'),
+                    new Index('Индекс 2', array('user_id'), null)
+                ),
+                'options' => array(
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ),
+            )
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ app/migrations/1.0.2/token.php
@@ -0,0 +1,116 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class TokenMigration_102
+ */
+class TokenMigration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('token', array(
+                'columns' => array(
+                    new Column(
+                        'id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        )
+                    ),
+                    new Column(
+                        'type',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'default' => "access",
+                            'notNull' => true,
+                            'size' => 50,
+                            'after' => 'id'
+                        )
+                    ),
+                    new Column(
+                        'value',
+                        array(
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 50,
+                            'after' => 'type'
+                        )
+                    ),
+                    new Column(
+                        'user_id',
+                        array(
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'value'
+                        )
+                    ),
+                    new Column(
+                        'created_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'user_id'
+                        )
+                    ),
+                    new Column(
+                        'updated_at',
+                        array(
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "CURRENT_TIMESTAMP",
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'created_at'
+                        )
+                    )
+                ),
+                'indexes' => array(
+                    new Index('PRIMARY', array('id'), 'PRIMARY'),
+                    new Index('FK_token_user', array('user_id'), null)
+                ),
+                'options' => array(
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '14',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8_general_ci'
+                ),
+            )
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
